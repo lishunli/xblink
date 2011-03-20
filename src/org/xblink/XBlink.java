32507eb5c6f08fe08bfa250a51419395cf38a410
pangwu86 <pangwu86@bca064e8-805a-a692-96ec-197f8323d3fd>
2011-03-20 12:11:27 +0000

    分支0.8.0 对0.7.0的精简版

--- src/org/xblink/XBlink.java
+++ src/org/xblink/XBlink.java
@@ -1,327 +1,343 @@
 package org.xblink;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 
-import org.xblink.core.XBlinkUtil;
-import org.xblink.util.IOUtil;
+import org.xblink.reader.XMLReader;
+import org.xblink.writer.XMLWriter;
 
 /**
- * XBlink(吾爱跳刀)，通用序列化反序列化工具集。<BR>
- * 
- * 支持JAVA中的基本类型，对象类型，数组类型，List，Set，Map等集合类型。<BR>
- * 
- * 支持生成XML，JSON，YAML,及任意你喜欢的格式文件。（当然非默认格式的输出需要你加入对应的实现）<BR>
+ * XBlink(吾爱跳刀),XML序列化反序列化工具集.<BR/>
+ * 支持基本类型，对象类型，数组类型，List，Set，Map等集合类型.
  * 
  * 
- * @author pangwu86@gmail.com
- * 
+ * @author pangwu86(pangwu86@gmail.com)
  */
-public abstract class XBlink {
-
-	// ****** 通用序列化方法 ******
+public class XBlink {
 
 	/**
-	 * toAny意味着You will get what you want（你会得到任何你想要的东西）<BR>
+	 * 序列化一个对象，生成XML文件.
 	 * 
-	 * XBlink最NB的方法，生成任意你想要的格式字符串。
-	 * 
+	 * @param filePath
+	 *            文件路径
 	 * @param obj
-	 *            需要被序列化对象
-	 * @param xbConfig
-	 *            XBlink配置信息
-	 * @param wanted
-	 *            任意你想要的格式名称，例如EXCEL或者XML
+	 *            实例对象
+	 */
+	public static void toXml(String filePath, Object obj) {
+		new XMLWriter().writeXML(getOutputStream(filePath), obj);
+	}
+
+	/**
+	 * 序列化一个对象，生成XML文件.
+	 * 
+	 * @param outputStream
+	 *            包含文件信息的输出流
+	 * @param obj
+	 *            实例对象
+	 */
+	public static void toXml(OutputStream outputStream, Object obj) {
+		new XMLWriter().writeXML(outputStream, obj);
+	}
+
+	/**
+	 * 反序列化一个对象，源自XML文件.
+	 * 
+	 * @param filePath
+	 *            文件路径
+	 * @param clz
+	 *            生成对象的Class
+	 * @return 对象
+	 */
+	public static Object fromXml(String filePath, Class<?> clz) {
+		return new XMLReader().readXML(getInputStream(filePath), clz, null);
+	}
+
+	/**
+	 * 反序列化一个对象，源自XML文件.
+	 * 
+	 * @param filePath
+	 *            文件路径
+	 * @param clzs
+	 *            生成对象的Class(Map集合使用，第一个参数是Key的类型，第二个参数是Value的类型)
+	 * @return 对象
+	 */
+	public static Object fromXml(String filePath, Class<?>[] clzs) {
+		return new XMLReader().readXML(getInputStream(filePath), clzs, null);
+	}
+
+	/**
+	 * 反序列化一个对象，源自XML文件.
+	 * 
+	 * @param filePath
+	 *            文件路径
+	 * @param clz
+	 *            生成对象的Class
+	 * @param implClasses
+	 *            生成对象中包含的所有接口实现类
+	 * @return 对象
+	 */
+	public static Object fromXml(String filePath, Class<?> clz, Class<?>[] implClasses) {
+		return new XMLReader().readXML(getInputStream(filePath), clz, implClasses, null);
+	}
+
+	/**
+	 * 反序列化一个对象，源自XML文件.
+	 * 
+	 * @param filePath
+	 *            文件路径
+	* @param clzs
+	 *            生成对象的Class(Map集合使用，第一个参数是Key的类型，第二个参数是Value的类型)
+	 * @param implClasses
+	 *            生成对象中包含的所有接口实现类
+	 * @return 对象
+	 */
+	public static Object fromXml(String filePath, Class<?>[] clzs, Class<?>[] implClasses) {
+		return new XMLReader().readXML(getInputStream(filePath), clzs, implClasses, null);
+	}
+
+	/**
+	 * 反序列化一个对象，源自XML文件.
+	 * 
+	 * @param inputStream
+	 *            包含文件信息的输 入流
+	 * @param clz
+	 *            生成对象的Class
+	 * @return 对象
+	 */
+	public static Object fromXml(InputStream inputStream, Class<?> clz) {
+		return new XMLReader().readXML(inputStream, clz, null);
+	}
+
+	/**
+	 * 反序列化一个对象，源自XML文件.
+	 * 
+	 * @param inputStream
+	 *            包含文件信息的输 入流
+	 * @param clzs
+	 *            生成对象的Class(Map集合使用，第一个参数是Key的类型，第二个参数是Value的类型)
+	 * @return 对象
+	 */
+	public static Object fromXml(InputStream inputStream, Class<?>[] clzs) {
+		return new XMLReader().readXML(inputStream, clzs, null);
+	}
+
+	/**
+	 * 反序列化一个对象，源自XML文件.
+	 * 
+	 * @param inputStream
+	 *            包含文件信息的输 入流
+	 * @param clz
+	 *            生成对象的Class
+	 * @param implClasses
+	 *            生成对象中包含的所有接口实现类
+	 * @return 对象
+	 */
+	public static Object fromXml(InputStream inputStream, Class<?> clz, Class<?>[] implClasses) {
+		return new XMLReader().readXML(inputStream, clz, implClasses, null);
+	}
+
+	/**
+	 * 反序列化一个对象，源自XML文件.
+	 * 
+	 * @param inputStream
+	 *            包含文件信息的输 入流
+	 * @param clzs
+	 *            生成对象的Class(Map集合使用，第一个参数是Key的类型，第二个参数是Value的类型)
+	 * @param implClasses
+	 *            生成对象中包含的所有接口实现类
+	 * @return 对象
+	 */
+	public static Object fromXml(InputStream inputStream, Class<?>[] clzs, Class<?>[] implClasses) {
+		return new XMLReader().readXML(inputStream, clzs, implClasses, null);
+	}
+
+	/**
+	 * 反序列化一个对象，源自XML文件.
+	 * 
+	 * @param filePath
+	 *            文件路径
+	 * @param clz
+	 *            生成对象的Class
+	 * @param classLoader
+	 *            用户的类加载器
+	 * @return 对象
+	 */
+	public static Object fromXml(String filePath, Class<?> clz, ClassLoader classLoader) {
+		return new XMLReader().readXML(getInputStream(filePath), clz, classLoader);
+	}
+
+	/**
+	 * 
+	 * 反序列化一个对象，源自XML文件.
+	 * 
+	 * @param filePath
+	 *            文件路径
+	 * @param clzs
+	 *            生成对象的Class(Map集合使用，第一个参数是Key的类型，第二个参数是Value的类型)
+	 * @param classLoader
+	 *            用户的类加载器
+	 * @return 对象
+	 */
+	public static Object fromXml(String filePath, Class<?>[] clzs, ClassLoader classLoader) {
+		return new XMLReader().readXML(getInputStream(filePath), clzs, classLoader);
+	}
+
+	/**
+	 * 反序列化一个对象，源自XML文件.
+	 * 
+	 * @param filePath
+	 *            文件路径
+	 * @param clz
+	 *            生成对象的Class
+	 * @param implClasses
+	 *            生成对象中包含的所有接口实现类
+	 * @param classLoader
+	 *            用户的类加载器
+	 * @return 对象
+	 */
+	public static Object fromXml(String filePath, Class<?> clz, Class<?>[] implClasses,
+			ClassLoader classLoader) {
+		return new XMLReader().readXML(getInputStream(filePath), clz, implClasses, classLoader);
+	}
+
+	/**
+	 * 反序列化一个对象，源自XML文件.
+	 * 
+	 * @param filePath
+	 *            文件路径
+	 * @param clzs
+	 *            生成对象的Class(Map集合使用，第一个参数是Key的类型，第二个参数是Value的类型)
+	 * @param implClasses
+	 *            生成对象中包含的所有接口实现类
+	 * @param classLoader
+	 *            用户的类加载器
+	 * @return 对象
+	 */
+	public static Object fromXml(String filePath, Class<?>[] clzs, Class<?>[] implClasses,
+			ClassLoader classLoader) {
+		return new XMLReader().readXML(getInputStream(filePath), clzs, implClasses, classLoader);
+	}
+
+	/**
+	 * 反序列化一个对象，源自XML文件.
+	 * 
+	 * @param inputStream
+	 *            包含文件信息的输 入流
+	 * @param clz
+	 *            生成对象的Class
+	 * @param classLoader
+	 *            用户的类加载器
+	 * @return 对象
+	 */
+	public static Object fromXml(InputStream inputStream, Class<?> clz, ClassLoader classLoader) {
+		return new XMLReader().readXML(inputStream, clz, classLoader);
+	}
+
+	/**
+	 * 反序列化一个对象，源自XML文件.
+	 * 
+	 * @param inputStream
+	 *            包含文件信息的输 入流
+	* @param clzs
+	 *            生成对象的Class(Map集合使用，第一个参数是Key的类型，第二个参数是Value的类型)
+	 * @param classLoader
+	 *            用户的类加载器
+	 * @return 对象
+	 */
+	public static Object fromXml(InputStream inputStream, Class<?>[] clzs, ClassLoader classLoader) {
+		return new XMLReader().readXML(inputStream, clzs, classLoader);
+	}
+
+	/**
+	 * 反序列化一个对象，源自XML文件.
+	 * 
+	 * @param inputStream
+	 *            包含文件信息的输 入流
+	 * @param clz
+	 *            生成对象的Class
+	 * @param implClasses
+	 *            生成对象中包含的所有接口实现类
+	 * @param classLoader
+	 *            用户的类加载器
+	 * @return 对象
+	 */
+	public static Object fromXml(InputStream inputStream, Class<?> clz, Class<?>[] implClasses,
+			ClassLoader classLoader) {
+		return new XMLReader().readXML(inputStream, clz, implClasses, classLoader);
+	}
+
+	/**
+	 * 反序列化一个对象，源自XML文件.
+	 * 
+	 * @param inputStream
+	 *            包含文件信息的输 入流
+	 * @param clzs
+	 *            生成对象的Class(Map集合使用，第一个参数是Key的类型，第二个参数是Value的类型)
+	 * @param implClasses
+	 *            生成对象中包含的所有接口实现类
+	 * @param classLoader
+	 *            用户的类加载器
+	 * @return 对象
+	 */
+	public static Object fromXml(InputStream inputStream, Class<?>[] clzs, Class<?>[] implClasses,
+			ClassLoader classLoader) {
+		return new XMLReader().readXML(inputStream, clzs, implClasses, classLoader);
+	}
+
+	/**
+	 * 清空当前环境中的缓存.
+	 */
+	public static void cleanCache() {
+		XMLObject.cleanCache();
+	}
+
+	/**
+	 * 获得输入流.
+	 * 
+	 * @param filePath
 	 * @return
 	 */
-	public static String toAny(Object obj, XBConfig xbConfig, String wanted) {
-		return XBlinkUtil.toAny(obj, xbConfig, null, wanted);
+	private static InputStream getInputStream(String filePath) {
+		try {
+			return new BufferedInputStream(new FileInputStream(new File(filePath)));
+		} catch (FileNotFoundException e) {
+			throw new RuntimeException("没有找到对应的文件", e);
+		}
 	}
 
 	/**
-	 * toAny意味着You will get what you want（你会得到任何你想要的东西）<BR>
+	 * 获得输入流.
 	 * 
-	 * XBlink最NB的方法，生成任意你想要的格式文件。
-	 * 
-	 * @param obj
-	 *            需要被序列化对象
-	 * @param xbConfig
-	 *            XBlink配置信息
 	 * @param filePath
-	 *            保存文件的路径
-	 * @param wanted
-	 *            任意你想要的格式名称，例如EXCEL或者XML
 	 * @return
 	 */
-	public static String toAny(Object obj, XBConfig xbConfig, String filePath, String wanted) {
-		return toAny(obj, xbConfig, IOUtil.getOutputStream(filePath), wanted);
+	private static OutputStream getOutputStream(String filePath) {
+		try {
+			return new BufferedOutputStream(new FileOutputStream(getFile(filePath)));
+		} catch (Exception e) {
+			throw new RuntimeException("没有找到对应的文件", e);
+		}
 	}
 
 	/**
-	 * toAny意味着You will get what you want（你会得到任何你想要的东西）<BR>
+	 * 获得文件对象.
 	 * 
-	 * XBlink最NB的方法，生成任意你想要的格式文件。
-	 * 
-	 * @param obj
-	 *            需要被序列化对象
-	 * @param xbConfig
-	 *            XBlink配置信息
-	 * @param outputStream
-	 *            输出流
-	 * @param wanted
-	 *            任意你想要的格式名称，例如EXCEL或者XML
+	 * @param filePath
 	 * @return
+	 * @throws IOException
 	 */
-	public static String toAny(Object obj, XBConfig xbConfig, OutputStream outputStream,
-			String wanted) {
-		return XBlinkUtil.toAny(obj, xbConfig, outputStream, wanted);
+	private static File getFile(String filePath) throws Exception {
+		File file = new File(filePath);
+		if (!file.exists()) {
+			file.createNewFile();
+		}
+		return file;
 	}
-
-	// ******　XML序列化方法　××××××
-
-	/**
-	 * 生成XML字符串。（默认配置）
-	 * 
-	 * @param obj
-	 *            需要被序列化对象
-	 * @return XML字符串
-	 */
-	public static String toXML(Object obj) {
-		return XBlinkUtil.toXML(obj, null, null);
-	}
-
-	/**
-	 * 生成XML字符串。
-	 * 
-	 * @param obj
-	 *            需要被序列化对象
-	 * @param xbConfig
-	 *            XBlink配置信息
-	 * @return XML字符串
-	 */
-	public static String toXML(Object obj, XBConfig xbConfig) {
-		return XBlinkUtil.toXML(obj, xbConfig, null);
-	}
-
-	/**
-	 * 生成XML文件。（默认配置）
-	 * 
-	 * @param obj
-	 *            需要被序列化对象
-	 * @param filePath
-	 *            保存文件的路径
-	 * @return XML字符串
-	 */
-	public static String toXML(Object obj, String filePath) {
-		return XBlinkUtil.toXML(obj, null, IOUtil.getOutputStream(filePath));
-	}
-
-	/**
-	 * 生成XML文件。（默认配置）
-	 * 
-	 * @param obj
-	 *            需要被序列化对象
-	 * @param outputStream
-	 *            输出流
-	 * @return XML字符串
-	 */
-	public static String toXML(Object obj, OutputStream outputStream) {
-		return XBlinkUtil.toXML(obj, null, IOUtil.getOutputStream(outputStream));
-	}
-
-	/**
-	 * 生成XML文件。
-	 * 
-	 * @param obj
-	 *            需要被序列化对象
-	 * @param xbConfig
-	 *            XBlink配置信息
-	 * @param filePath
-	 *            保存文件的路径
-	 * @return XML字符串
-	 */
-	public static String toXML(Object obj, XBConfig xbConfig, String filePath) {
-		return XBlinkUtil.toXML(obj, xbConfig, IOUtil.getOutputStream(filePath));
-	}
-
-	/**
-	 * 生成XML文件。
-	 * 
-	 * @param obj
-	 *            需要被序列化对象
-	 * @param xbConfig
-	 *            XBlink配置信息
-	 * @param outputStream
-	 *            输出流
-	 * @return XML字符串
-	 */
-	public static String toXML(Object obj, XBConfig xbConfig, OutputStream outputStream) {
-		return XBlinkUtil.toXML(obj, xbConfig, IOUtil.getOutputStream(outputStream));
-	}
-
-	// ******　JSON序列化方法　××××××
-
-	/**
-	 * 生成JSON字符串。（默认配置）
-	 * 
-	 * @param obj
-	 *            需要被序列化对象
-	 * @return JSON字符串
-	 */
-	public static String toJSON(Object obj) {
-		return XBlinkUtil.toJSON(obj, null, null);
-	}
-
-	/**
-	 * 生成JSON字符串。
-	 * 
-	 * @param obj
-	 *            需要被序列化对象
-	 * @param xbConfig
-	 *            XBlink配置信息
-	 * @return JSON字符串
-	 */
-	public static String toJSON(Object obj, XBConfig xbConfig) {
-		return XBlinkUtil.toJSON(obj, xbConfig, null);
-	}
-
-	/**
-	 * 生成JSON文件。（默认配置）
-	 * 
-	 * @param obj
-	 *            需要被序列化对象
-	 * @param filePath
-	 *            保存文件的路径
-	 * @return JSON字符串
-	 */
-	public static String toJSON(Object obj, String filePath) {
-		return XBlinkUtil.toJSON(obj, null, IOUtil.getOutputStream(filePath));
-	}
-
-	/**
-	 * 生成JSON文件。（默认配置）
-	 * 
-	 * @param obj
-	 *            需要被序列化对象
-	 * @param outputStream
-	 *            输出流
-	 * @return JSON字符串
-	 */
-	public static String toJSON(Object obj, OutputStream outputStream) {
-		return XBlinkUtil.toJSON(obj, null, IOUtil.getOutputStream(outputStream));
-	}
-
-	/**
-	 * 生成JSON文件。
-	 * 
-	 * @param obj
-	 *            需要被序列化对象
-	 * @param xbConfig
-	 *            XBlink配置信息
-	 * @param filePath
-	 *            保存文件的路径
-	 * @return JSON字符串
-	 */
-	public static String toJSON(Object obj, XBConfig xbConfig, String filePath) {
-		return XBlinkUtil.toJSON(obj, xbConfig, IOUtil.getOutputStream(filePath));
-	}
-
-	/**
-	 * 生成JSON文件。
-	 * 
-	 * @param obj
-	 *            需要被序列化对象
-	 * @param xbConfig
-	 *            XBlink配置信息
-	 * @param outputStream
-	 *            输出流
-	 * @return JSON字符串
-	 */
-	public static String toJSON(Object obj, XBConfig xbConfig, OutputStream outputStream) {
-		return XBlinkUtil.toJSON(obj, xbConfig, IOUtil.getOutputStream(outputStream));
-	}
-
-	// ******　YAML序列化方法　××××××
-
-	/**
-	 * 生成YAML字符串。（默认配置）
-	 * 
-	 * @param obj
-	 *            需要被序列化对象
-	 * @return YAML字符串
-	 */
-	public static String toYAML(Object obj) {
-		return XBlinkUtil.toYAML(obj, null, null);
-	}
-
-	/**
-	 * 生成YAML字符串。
-	 * 
-	 * @param obj
-	 *            需要被序列化对象
-	 * @param xbConfig
-	 *            XBlink配置信息
-	 * @return YAML字符串
-	 */
-	public static String toYAML(Object obj, XBConfig xbConfig) {
-		return XBlinkUtil.toYAML(obj, xbConfig, null);
-	}
-
-	/**
-	 * 生成YAML文件。（默认配置）
-	 * 
-	 * @param obj
-	 *            需要被序列化对象
-	 * @param filePath
-	 *            保存文件的路径
-	 * @return YAML字符串
-	 */
-	public static String toYAML(Object obj, String filePath) {
-		return XBlinkUtil.toYAML(obj, null, IOUtil.getOutputStream(filePath));
-	}
-
-	/**
-	 * 生成YAML文件。（默认配置）
-	 * 
-	 * @param obj
-	 *            需要被序列化对象
-	 * @param outputStream
-	 *            输出流
-	 * @return YAML字符串
-	 */
-	public static String toYAML(Object obj, OutputStream outputStream) {
-		return XBlinkUtil.toYAML(obj, null, IOUtil.getOutputStream(outputStream));
-	}
-
-	/**
-	 * 生成YAML文件。
-	 * 
-	 * @param obj
-	 *            需要被序列化对象
-	 * @param xbConfig
-	 *            XBlink配置信息
-	 * @param filePath
-	 *            保存文件的路径
-	 * @return YAML字符串
-	 */
-	public static String toYAML(Object obj, XBConfig xbConfig, String filePath) {
-		return XBlinkUtil.toYAML(obj, xbConfig, IOUtil.getOutputStream(filePath));
-	}
-
-	/**
-	 * 生成YAML文件。
-	 * 
-	 * @param obj
-	 *            需要被序列化对象
-	 * @param xbConfig
-	 *            XBlink配置信息
-	 * @param outputStream
-	 *            输出流
-	 * @return YAML字符串
-	 */
-	public static String toYAML(Object obj, XBConfig xbConfig, OutputStream outputStream) {
-		return XBlinkUtil.toYAML(obj, xbConfig, IOUtil.getOutputStream(outputStream));
-	}
-
-	// XXX 反序列化方法
-
 }
