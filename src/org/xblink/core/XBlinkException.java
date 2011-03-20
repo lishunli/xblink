2ac2f46f3ecfe5697c6d1df6fb63ef3c06143a72
pangwu86 <pangwu86@bca064e8-805a-a692-96ec-197f8323d3fd>
2011-03-20 12:16:04 +0000

    重构版移动到 branches下

---
+++ src/org/xblink/core/XBlinkException.java
@@ -0,0 +1,16 @@
+package org.xblink.core;
+
+/**
+ * XBlink异常类
+ * @author E-Hunter(xjf1986518@gmail.com)
+ *
+ */
+public class XBlinkException extends Exception {
+
+	public XBlinkException(){
+		
+	}
+	public XBlinkException(String msg){
+		super(msg);
+	}
+}
